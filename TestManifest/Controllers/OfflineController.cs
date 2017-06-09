78b7d45add330b94004240e9d614dd09934091e6
radu negrila <radu.negrila@gmail.com>
2017-06-09 17:02:51 +0300

    first checkin

---
+++ TestManifest/Controllers/OfflineController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using TestManifest.Manifest;
+
+namespace TestManifest.Controllers
+{
+    public class OfflineController : Controller
+    {
+        public ActionResult Start()
+        {
+            return View();
+        }
+
+        public ActionResult Report()
+        {
+            return View();
+        }
+
+        public ActionResult Manifest()
+        {
+            var manifestResult = new ManifestResult("1.0")
+            {
+                CacheResources = new List<string>
+                {
+                    Url.Action("Start", "Offline"),
+                    Url.Action("Report", "Offline"),
+                    Url.Content("~/content/style.css"),
+                    Url.Content("~/scripts/main.js"),
+                    Url.Content("~/content/images/logo_offline.png"),
+                },
+                NetworkResources = new[] {Url.Action("Index", "Home")},
+                FallbackResources =
+                {
+                    {Url.Content("~/content/images/logo_online.png"), Url.Content("~/content/images/logo_offline.png") }
+                }
+            };
+            return manifestResult;
+        }
+    }
+}
---
+++ TestManifest/Scripts/main.js
@@ -0,0 +1,11 @@
+﻿window.addEventListener('offline',
+    function (e) {
+        document.getElementById("logo").setAttribute("src", "/TestManifest/Content/images/logo_offline.png");
+    },
+    false);
+
+window.addEventListener('online',
+    function (e) {
+        document.getElementById("logo").setAttribute("src", "/TestManifest/Content/images/logo_online.png");
+    },
+    false);
